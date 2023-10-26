f53e7b143386a94cd8ea07fe94983c8f5fe93cff
HARMIN <132663894+harminvp00@users.noreply.github.com>
2023-10-26 18:32:54 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,35 +1,27 @@
 
-questions = [
-    "Which language was used to create Facebook?",
-    "Where is Goverment Polytechnic Gandhinagar?"
-]
+# in this game player can use lifelines only 2 times
+# This function show correct answer for corrent question to the player
+def lifeline(a):
+    print('Right Answer is :',a)
 
-option = [
-    ['python','english','javascript','php', 3],
-    ['Ahmedabad', 'Gandhinagar', 'Baroda', 'Rajkot', 2]
-]
-
-reward = [10000,20000,50000,100000,250000,500000,1000000]
-i=0
-for i in range(i,len(questions)):
-    print(questions[i])
-
-    print(f'a.{option[i][0]}        b.{option[i][1]}')
-    print(f'c.{option[i][2]}        d.{option[i][3]}')
-
-    reply= int(input('\nEnter answer here:'))
+# Master_Level function save players achivement if level > Master_level
+# by this function of code player not lost all game 
+def Master_Level(lev):
+    Master_lev=0
+    if lev >= 4:
+      Master_lev=1
+    if lev >= 9:
+      Master_lev=2
+    if lev >= 12:
+      Master_lev=3
+    if lev >= 14:
+      Master_lev=4
+    return Master_lev
     
-    if( reply == option[i][-1]):
-        print(f'correct you have won {reward[i]}\n\n')
-    else:
-        print('OOPS, The Wrong Answer!')
-        print('|| Game Over || ("=")\n\n')
-        break
-   #by vinay :
-print(
-  "Welcome to KAUN BANEGA CROREPATI quiz by vinay vaishnav\n\t\"Enter the correct option name in lowercase\""
-)
-question_list = [
+    
+# main coding 
+# List is used here to store questions
+questions = [
   "1.Indian felmale politician and lawyer who served as the 12th president of India ",
   "2.Which Indian became  Prime Minister of Uk on 25 October 2022?",
   "3.Where is qutub minar?",
@@ -37,65 +29,89 @@ question_list = [
   "5.Which battle in 1757 marked the beginning of British occupation in India??",
   "6.The weight of diamonds is usually measured in what?",
   "7.Who is the prime minister of pakistan?",
-  "8.Who is the bowler of cricket?", "9.What is the capital of bangladesh?",
+  "8.Who is the bowler of cricket?",
+  "9.What is the capital of bangladesh?",
   "10.Where is new york?",
   "11.National Mathematics Day is observed annually on ",
   "12.On which of the following days is world computer literacy  day observed?"
 ]
+
+# all_option  = [ a.first_option    b.second_option ]
+#               [ c.third_option    d.fourth_option ]
+
+# Option A for All Questions
 first_option = [
   "a.Anandiben Patel", "a.Rishi Sunak ", "a.Mumbai", "a.Dirghaayu",
   "a.Plassey", "a.Tola", "a.Imran khan", "a.Murli vijay", "a.turkey", "a.UK",
   "a.December 22", "a.March 28"
 ]
+
+# Option B for All Questions
 second_option = [
   "b.Droupadi Murmu", "b.Liz Truss", "b.Delhi", "b.Chiranjeevi", "b.Buxar",
   "b.Carat", "b.Shehbaz Sharif", "b.Ishant Sharma", "b.Islamabad", "b.USA",
   "b.July 18", "b.May 31"
 ]
+
+# Option C for All Questions
 third_option = [
 "c.Prathibha DeviSingh Patil", "c.Boris Johnson", "c.Himachal Pradesh",
-"c.Suhagan", "c.Assaye", "c.Maund", "c.Nawaz Sarif", "c.Mickal Clarke",]
+"c.Suhagan", "c.Assaye", "c.Maund", "c.Nawaz Sarif", "c.Mickal Clarke",
 "c.Dhaka", "c.India", "c.June 22", "c.December 2"
+]
 
+# Option D for All Questions
 fourth_option = [
   "d.Sumitra Sathe", "d.George Washington", "d.Kanpur", "d.Sushil",
   "d.Panipat", "d.Troy", "d.Narendra Modi", "d.Virat Kohli", "d.Delhi",
   "d.Pakistan", "d.March 14", "d.September 27"
 ]
-# all_option  = [first_option,second_option,third_option,fourth_option]
-ans_key = ["c", "a", "b", "c", "a", "b", "b", "b", "c", "b", "a", "c"]
-ans_list = []
-correct_answers = 0
 
-price = [
+# Right Answer for All Question
+answers = ["c", "a", "b", "c", "a", "b", "b", "b", "c", "b", "a", "c"]
+
+# Rewards which you get for each right answer
+reward = [
   1000, 2000, 3000, 5000, 10000, 200000, 40000, 80000, 160000, 320000, 640000,
-  1280000
+  1250000, 2500000, 5000000, 7500000, 10000000, 75000000
 ]
-#money of all answers.
-a = 0
-while a < len(question_list):
 
-  print(question_list[a])
-  print(first_option[a])
-  print(second_option[a])
-  print(third_option[a])
-  print(fourth_option[a])
-  user = input("enter the option name of your answer-")
-  if user == ans_key[a]:
-    print("Congrats! your answer is correct", "\t you won", price[a])
-    print("")
-    correct_answers = 1
-  else:
-    print("Sorry your answer is wrong")
-    print("")
-    break
-  a = a + 1
-  ans_list.append(user)
+# Variable declaration according to need in diffrent task     
+      
+i=0                     # it's value is used to display Question or options    
+count=2                 # it's indicates the avialable lifeline for player
+ll='no'                 # ll = lifeline , if player input 'yes' it can be show right answer of the question
+level=-1                # it is a simply level of the player
+Master_level=0          # if the player achive master level and lost the game 
+                        # then player not lost its all earned rewards but it was achive only his last master_level reward not all
 
-print(
-  f"Congrats! You Have  Completed Your Game with \nthe Price is {price[a-1]}",
-  " \nThanks For playing created by vinay vaishnav")
-        
-        
- 
 
+# This loop will be not terminate untill player enter wrong answer of the question     
+for i in range(i,len(questions)):
+    print('Master Level = Level',Master_level)
+    print('Available LineLines :',count,'\n')
+    print(questions[i])
+
+    print(f'{first_option[i]}           {second_option[i]}')
+    print(f'{third_option[i]}           {fourth_option[i]}')
+
+    # if player enter yes for two time then this condition never run for third time 
+    if count>0: 
+        ll=input("Want to use lifeline?? (yes or no) :-")
+        if  ll == 'yes':
+            count-=1
+            lifeline(answers[i])
+
+    # User Reply        
+    reply= input('\nEnter answer here:')
+    
+    # if reply is right answer then player level is increment also reward is showed
+    if reply == answers[i]:
+        level+=1
+        Master_level=Master_Level(level)
+        print(f'correct you have won {reward[i]}\n') 
+
+    # if answer is not right then code will be terminates
+    else:
+        print('OOPS, The Wrong Answer!')
+        break
